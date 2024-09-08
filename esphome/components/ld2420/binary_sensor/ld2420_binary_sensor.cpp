f8fd71a68a9dd7928099a86d3fe60ddce4337dbf
emanueloa <71952367+emanueloa@users.noreply.github.com>
2024-09-08 20:13:58 -0300

    Update ld2420_binary_sensor.cpp

--- esphome/components/ld2420/binary_sensor/ld2420_binary_sensor.cpp
+++ esphome/components/ld2420/binary_sensor/ld2420_binary_sensor.cpp
@@ -10,6 +10,7 @@ static const char *const TAG = "LD2420.binary_sensor";
 void LD2420BinarySensor::dump_config() {
   ESP_LOGCONFIG(TAG, "LD2420 BinarySensor:");
   LOG_BINARY_SENSOR("  ", "Presence", this->presence_bsensor_);
+  state = this->presence_bsensor_;
 }
 
 }  // namespace ld2420
